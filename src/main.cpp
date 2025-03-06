a5ec42c3c3e000823d8496c21678346e1a57e079
Pxlmastr <44294279+pxlmastrXD@users.noreply.github.com>
2025-03-06 11:48:01 -0500

    S Final Code Structure, ready to debug

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,29 @@ int getResult(Operations op) {
 
 
 int main(int argc, char* argv[]) {
-       auto op; // TODO: Parse args and make it into an Operations();
-       
+       auto num1 = (int) argv[1];
+       auto aop = argv[2];
+       auto num2 = (int) argv[3];
+       enum MathType oper;
+       switch (aop) {
+        case '+':
+            oper = MathType::ADD;
+            break;
+        case '-':
+            oper = MathType::SUB;
+            break;
+        case '*':
+            oper = MathType::MUL;
+            break;
+        case '/':
+            oper = MathType::DIV;
+            break;
+        default:
+            oper = MathType::OTHER;
+            break;
+       };
+       auto op = Operations(num1, num2, oper);
+       auto result = getResult(op);
+       std::cout << "Result: " << result << "\n";
+       return 0;
 }
